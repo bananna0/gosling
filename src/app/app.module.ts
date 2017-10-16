b97c6eaaed43d92c2b84accbe2d35ff168c03319
Anna Ivanova <klea94@mail.ru>
2017-10-16 19:56:20 +0300

    Add some component, try to use mdl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdlModule } from 'angular2-mdl';
 
 import { AppComponent } from './app.component';
 
