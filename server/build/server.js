c1259fdbe693c2893a122b1160a00e28b9481c27
Anna Ivanova <klea94@mail.ru>
2017-10-20 17:13:23 +0300

    Change app-structure, add simple server

---
+++ server/build/server.js
@@ -0,0 +1,18 @@
+"use strict";
+/* app/server.ts */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import everything from express and assign it to the express variable
+var express = require("express");
+// Import WelcomeController from controllers entry point
+var api_1 = require("./api");
+// Create a new express application instance
+var app = express();
+// The port the express app will listen on
+var port = 3000;
+// Mount the WelcomeController at the /welcome route
+app.use('/welcome', api_1.ContactController);
+// Serve the application at the given port
+app.listen(port, function () {
+    // Success callback
+    console.log("Listening at http://localhost:" + port + "/");
+});
